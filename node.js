36336bdff0dc3832bb1cfa7c24faed79403b5d7b
hubmartin <hub.martin@gmail.com>
2017-02-11 14:01:30 +0100

    Add lock and multiple paralel users

--- node.js
+++ node.js
@@ -11,7 +11,7 @@ var server = app.listen(port, function(err) {
   if (err) {
     return console.log('something bad happened', err);
   }
-  console.log('server is listening on ${port}');
+  console.log("Server is listening on localhost:" + port);
 })
 
 var fs = require('fs');
@@ -19,7 +19,7 @@ var io = require('socket.io')(server);
 var bodyParser = require('body-parser')
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  console.log('+ User connected ' + socket.id);
   
   socket.on('msg', function(msg){
     console.log('message: ' + msg);
@@ -29,53 +29,95 @@ io.on('connection', function(socket){
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-function print(text)
+function print(socketId, text)
 {
-	io.emit("msg", text);
+    try {
+    if(socketId)
+        io.sockets.connected[socketId].emit("msg", text);
+     }
+     catch(ex)
+     {
+        (ex);
+     }
+        
+	//io.emit("msg", text);
 	console.log(text);
 }
+
+function makeid()
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
 
 app.use('/', express.static('public'))
 
 app.post('/', function (req, res) {
 
+    var code = req.body.code;
+    var socketId = req.body.socketId;
+    
+    if(compiling)
+    {
+        print(socketId, "Another user is compiling, try it again later please.");
+        return;
+    }
+    
+    compiling = true;
 
-	print('Got a POST request<br />');
-	print("Saving file...");
-
-  
-	var code = req.body.code;
-  
+    
+    
+    //io.to(socketId).emit("msg", "pouze jednomu klientovi___________");
+    
+    //io.sockets.connected[socketId].emit("msg", "pouze jednomu klientovi___________");
+    
+    //io.sockets.socket(socketId).emit("msg", "pouze jednomu klientovi___________");
+    
+    print(socketId, "RX socketid: " + socketId);
+    
+	print(socketId, "Saving file...");
+    
 	fs.writeFile("../app/application.c", code, function(err) {
   
     if(err) {
         return console.log(err);
     }
 
-    print("The file was saved!");
+    print(socketId, "The file was saved!");
 	
 	var spawn = require('child_process').spawn;
 	var compile = spawn('make', ['all'], { cwd: ".."});
 
 	compile.stdout.on('data', function (data) {
-		print(String(data));
+		print(socketId, String(data));
 	});
 
 	compile.stderr.on('data', function (data) {
-		print(String(data));
+		print(socketId, String(data));
 	});
 
 	compile.on('close', function (data) {
 		if (data === 0) {
+        
+        var dstFilename = "firmware_" + makeid() + ".bin";
+        print(socketId, "Binary: " + dstFilename);
 		
-		fs.rename("../out/firmware.bin", "public/firmware.bin", function (err) {
+		fs.rename("../out/firmware.bin", "public/" + dstFilename, function (err) {
 			if (err) {
-				print("Copy error");
+				print(socketId, "Copy error");
 				return;
 			}
+            
+            compiling = false;
 			
-			print("Starting download");
-			res.redirect("firmware.bin");
+			print(socketId, "Starting download");
+			res.redirect(dstFilename);
 		});
 		
 		}
