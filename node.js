c3e605dbab9857cfb14fc47a26edf89df66a9226
hubmartin <hub.martin@gmail.com>
2017-02-11 21:58:59 +0100

    Add side panel, logo, examples, fix multiple socket events

--- node.js
+++ node.js
@@ -1,12 +1,20 @@
 
-const express = require('express');
-const app = express();
+
+// Configure port
 const port = 7989; //3000;
 
-var compiling = false;
 
+
+const fs = require('fs')
+const path = require('path')
+const express = require('express');
+const app = express();
+
+var compiling = false;
 var http = require('http').Server(app);
 
+
+// Create server
 var server = app.listen(port, function(err) {  
   if (err) {
     return console.log('something bad happened', err);
@@ -14,17 +22,44 @@ var server = app.listen(port, function(err) {
   console.log("Server is listening on localhost:" + port);
 })
 
-var fs = require('fs');
+
+
 var io = require('socket.io')(server);
 var bodyParser = require('body-parser')
 
+var oldSocket = null;
+
+// Define Socket.io messages
 io.on('connection', function(socket){
   console.log('+ User connected ' + socket.id);
-  
+ 
   socket.on('msg', function(msg){
     console.log('message: ' + msg);
   });
   
+  socket.on('cmd', function(cmd){
+    console.log('CMD: ' + cmd);
+  });
+  
+  socket.on('loadExample', function(id){
+    console.log('Loading: ' + id);
+    
+        var dirs = getDirectories(__dirname + "/../sdk/_examples");
+    
+        var example = dirs[id];
+        
+        console.log("Example: " + example);
+        
+        fs.readFile(__dirname + "/../sdk/_examples/" + example + "/application.c", "utf8", function (err,data) {
+          if (err) {
+            return console.log(err);
+          }
+      
+      console.log("Sending EMI server>client");
+      socket.emit("example", data);
+    });
+    
+  });  
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -34,20 +69,18 @@ function print(socketId, text)
     try {
     if(socketId)
         io.sockets.connected[socketId].emit("msg", text);
-     }
-     catch(ex)
-     {
-        (ex);
-     }
-        
-	//io.emit("msg", text);
-	console.log(text);
+    }
+    catch(ex) {(ex); }
+
+    console.log(text);
 }
 
+// Create random characters to append to generated firmware
+// This allows multiple clients to downlaod their own compiled firmwares at the same time
 function makeid()
 {
     var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-";
 
     for( var i=0; i < 5; i++ )
         text += possible.charAt(Math.floor(Math.random() * possible.length));
@@ -55,9 +88,35 @@ function makeid()
     return text;
 }
 
+
+function getDirectories (srcpath) {
+  return fs.readdirSync(srcpath)
+    .filter(file => fs.statSync(path.join(srcpath, file)).isDirectory())
+}
+
+// Timeout during compilation
 var timeout;
 
+// Serve static content from "static" directory
 app.use('/', express.static('public'))
+
+
+// Get list of examples
+// TODO: convert to Socket/AJAX
+app.get("/examples", function(req, res) {
+
+    var dirs = getDirectories(__dirname + "/../sdk/_examples");
+    console.log(dirs);
+    var str = "<h1 style=\"color: white;\">Can be loaded, not compiled yet</h1>";
+    
+    for(item in dirs)
+    {
+        // ugly hack
+        str += "<a href = \"#\" style=\"color:white\" onclick=\"parent.loadExample("+item+")\">" + dirs[item] + "</a><br / >"
+    }
+    res.send(str);
+});
+
 
 app.post('/', function (req, res) {
 
@@ -72,16 +131,8 @@ app.post('/', function (req, res) {
     
     compiling = true;
 
-    
-    
-    //io.to(socketId).emit("msg", "pouze jednomu klientovi___________");
-    
-    //io.sockets.connected[socketId].emit("msg", "pouze jednomu klientovi___________");
-    
-    //io.sockets.socket(socketId).emit("msg", "pouze jednomu klientovi___________");
-    
+  
     print(socketId, "RX socketid: " + socketId);
-    
 	print(socketId, "Saving file...");
     
 	fs.writeFile("../app/application.c", code, function(err) {
@@ -95,11 +146,12 @@ app.post('/', function (req, res) {
 	var spawn = require('child_process').spawn;
 	var compile = spawn('make', ['all'], { cwd: ".."});
     
+    // Timeout if something goes wrong during compilation
     timeout = setTimeout(function(){
         print(socketId, "Compilation is too long, timeout.");
         compile.kill();
         compiling = false;
-    }, 30000);
+    }, 60000);
 
 	compile.stdout.on('data', function (data) {
 		print(socketId, String(data));
@@ -138,9 +190,14 @@ app.post('/', function (req, res) {
 		});
 		
 		} else {
-            print(socketId, "Compilation too long, timeout.");
-            res.send("Compilation too long, timeout.");
+            print(socketId, "Compilation error");
+            res.send("Compilation error");
             compiling = false;
+            
+            try {
+                clearTimeout(timeout);
+            }
+            catch(ex) {}
         }
 	})
 	
